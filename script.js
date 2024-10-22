c3c92afec4db2c9560671e8ba83266364179b5fb
Denis Bazylchik <den.bazylchik@gmail.com>
2024-10-22 20:53:42 +0200

    Adding modules

--- script.js
+++ script.js
@@ -1,46 +1,4 @@
-const quotes = [
-  {
-    quote: 'The only way to do great work is to love what you do',
-    author: 'Steve Jobs',
-  },
-  {
-    quote: 'Innovation distinguishes between a leader and a follower',
-    author: 'Steve Jobs',
-  },
-  {
-    quote: "Your time is limited, so don't waste it living someone else's life",
-    author: 'Steve Jobs',
-  },
-  {
-    quote: 'Imagination is more important than knowledge',
-    author: 'Albert Einstein',
-  },
-  {
-    quote: 'It always seems impossible until it’s done',
-    author: 'Nelson Mandela',
-  },
-  {
-    quote: 'Be the change that you wish to see in the world',
-    author: 'Mahatma Gandhi',
-  },
-  {
-    quote: 'The way to get started is to quit talking and begin doing',
-    author: 'Walt Disney',
-  },
-  {
-    quote:
-      'Success is not final, failure is not fatal: It is the courage to continue that counts',
-    author: 'Winston Churchill',
-  },
-  {
-    quote: 'No one can make you feel inferior without your consent',
-    author: 'Eleanor Roosevelt',
-  },
-  {
-    quote: 'It does not matter how slowly you go as long as you do not stop',
-    author: 'Confucius',
-  },
-];
+import quotes from './quotes.js';
 
 const quoteElementQuote = document.getElementById('quote');
 const quoteElementAuthor = document.getElementById('author');
