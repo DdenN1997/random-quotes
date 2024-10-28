bb858b23e920034b9cfd065dac1cc04763236a3d
Denis Bazylchik <den.bazylchik@gmail.com>
2024-10-28 18:15:14 +0100

    Displaying favorite quotes in the cards.

--- quotes.js
+++ quotes.js
@@ -2,43 +2,53 @@ const quotes = [
   {
     quote: 'The only way to do great work is to love what you do',
     author: 'Steve Jobs',
+    isFavorite: false,
   },
   {
     quote: 'Innovation distinguishes between a leader and a follower',
     author: 'Steve Jobs',
+    isFavorite: false,
   },
   {
     quote: "Your time is limited, so don't waste it living someone else's life",
     author: 'Steve Jobs',
+    isFavorite: false,
   },
   {
     quote: 'Imagination is more important than knowledge',
     author: 'Albert Einstein',
+    isFavorite: false,
   },
   {
     quote: 'It always seems impossible until it’s done',
     author: 'Nelson Mandela',
+    isFavorite: false,
   },
   {
     quote: 'Be the change that you wish to see in the world',
     author: 'Mahatma Gandhi',
+    isFavorite: false,
   },
   {
     quote: 'The way to get started is to quit talking and begin doing',
     author: 'Walt Disney',
+    isFavorite: false,
   },
   {
     quote:
       'Success is not final, failure is not fatal: It is the courage to continue that counts',
     author: 'Winston Churchill',
+    isFavorite: false,
   },
   {
     quote: 'No one can make you feel inferior without your consent',
     author: 'Eleanor Roosevelt',
+    isFavorite: false,
   },
   {
     quote: 'It does not matter how slowly you go as long as you do not stop',
     author: 'Confucius',
+    isFavorite: false,
   },
 ];
 
--- script.js
+++ script.js
@@ -3,13 +3,49 @@ import quotes from './quotes.js';
 const quoteElementQuote = document.getElementById('quote');
 const quoteElementAuthor = document.getElementById('author');
 const generateBtn = document.getElementById('generate-btn');
+const toggleFavoriteBtn = document.getElementById('toggle-favorite-btn');
+const favoritesContainer = document.getElementById('favorites-container');
+
+let currentQuoteIndex;
 
 function generateRandomQuote() {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
+  currentQuoteIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[currentQuoteIndex];
   const { quote, author } = randomQuote;
   quoteElementQuote.textContent = quote;
   quoteElementAuthor.textContent = author;
+
+  toggleFavoriteBtn.textContent = randomQuote.isFavorite
+    ? 'Remove from favorite'
+    : 'Add to favorite';
+
+  toggleFavoriteBtn.style.display = 'inline-block';
+}
+
+function toggleFavorite() {
+  if (currentQuoteIndex !== undefined) {
+    const currentQuote = quotes[currentQuoteIndex];
+    currentQuote.isFavorite = !currentQuote.isFavorite;
+    toggleFavoriteBtn.textContent = currentQuote.isFavorite
+      ? 'Remove to favorite'
+      : 'Add to favorite';
+
+    if (currentQuote.isFavorite) {
+      const favoritesCard = document.createElement('div');
+      favoritesCard.classList.add('favorite-card');
+      favoritesCard.innerHTML = `<p>"${currentQuote.quote}"</p>
+  <p class="author">${currentQuote.author}</p>`;
+      favoritesContainer.appendChild(favoritesCard);
+    } else {
+      const favoritesCards = document.querySelectorAll('.favorite-card');
+      favoritesCards.forEach((card) => {
+        if (card.textContent.includes(currentQuote.quote)) {
+          card.remove();
+        }
+      });
+    }
+  }
 }
 
 generateBtn.addEventListener('click', generateRandomQuote);
+toggleFavoriteBtn.addEventListener('click', toggleFavorite);
